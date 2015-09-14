faf6e046affbe471753b22f593f33370d5335db8
Inooid <justdrawcats@gmail.com>
2015-09-14 11:26:36 +0200

    Add show method for projects controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,9 +2,21 @@ module Api
   module V1
     # Handles the projects API endpoint
     class ProjectsController < ApplicationController
+      before_action :set_project, only: [:show, :update, :destroy]
+
       def index
         @projects = Project.all
         render json: @projects
+      end
+
+      def show
+        render json: @project
+      end
+
+      private
+
+      def set_project
+        @project = Project.find(params[:id])
       end
     end
   end
